7401c9a0af153dab05e1288e3c84eb17b9367d77
Alex Hooten <ah00t13@rams.colostate.edu>
2020-09-02 12:00:25 -0600

    added about skeleton

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -12,6 +12,34 @@ export default class About extends Component {
           <Row>
             <Col>
               <h2>{CLIENT_TEAM_NAME}</h2>
+              <h3>Our Mission</h3>
+              <p>Mission statement</p>
+              <h3>Our Team</h3>
+              <div class="member">
+                <h4>Member 1</h4>
+                <img src=""></img>
+                <p>Member 1 Bio</p>
+              </div>
+              <div className="member">
+                <h4>Member 2</h4>
+                <img src=""></img>
+                <p>Member 2 Bio</p>
+              </div>
+              <div className="member">
+                <h4>Member 3</h4>
+                <img src=""></img>
+                <p>Member 3 Bio</p>
+              </div>
+              <div className="member">
+                <h4>Member 4</h4>
+                <img src=""></img>
+                <p>Member 4 Bio</p>
+              </div>
+              <div className="member">
+                <h4>Member 5</h4>
+                <img src=""></img>
+                <p>Member 5 Bio</p>
+              </div>
             </Col>
             <Col id="closeAbout" xs='auto' >
               <Button color="primary" onClick={this.props.closePage} xs={1}>
