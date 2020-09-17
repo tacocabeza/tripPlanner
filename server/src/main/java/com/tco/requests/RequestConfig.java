4e8122517c09a9dcbab39a4ac2e95da1325841a2
suyashhiray <46005766+suyashhiray@users.noreply.github.com>
2020-09-17 12:35:28 -0700

    Update RequestConfig.java
    Added supportedRequests array

--- server/src/main/java/com/tco/requests/RequestConfig.java
+++ server/src/main/java/com/tco/requests/RequestConfig.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 public class RequestConfig extends RequestHeader {
 
   private String serverName;
+  private String[] supportedRequests = new String[3];
   private final transient Logger log = LoggerFactory.getLogger(RequestConfig.class);
 
   public RequestConfig() {
@@ -17,6 +18,9 @@ public class RequestConfig extends RequestHeader {
   @Override
   public void buildResponse() {
     this.serverName = "t01 Feather Friends";
+    this.supportedRequests[0] = "config";
+    this.supportedRequests[0] = "distance";
+    this.supportedRequests[0] = "find";
     log.trace("buildResponse -> {}", this);
   }
 
