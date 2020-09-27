638aced4c8c29e9aa21f31cd6425e8e276a1670b
Jesus Garcia <jgarc110@rams.colostate.edu>
2020-09-26 19:06:05 -0600

    remove testing functions

--- server/src/main/java/com/tco/requests/RequestDistance.java
+++ server/src/main/java/com/tco/requests/RequestDistance.java
@@ -24,19 +24,6 @@ public class RequestDistance extends RequestHeader{
         this.place2 = new HashMap<String,String>();
         this.requestVersion = RequestHeader.CURRENT_SUPPORTED_VERSION;
     }
-    // never called but for testing purposes
-    public RequestDistance(Float radius, String lat1, String lon1, String lat2, String lon2){
-        this();
-        this.distance = null;
-        this.earthRadius = radius;
-        this.place1 = new HashMap<String,String>();
-        this.place1.put("latitude", lat1);
-        this.place1.put("longitude", lon1);
-
-        this.place2 = new HashMap<String,String>();
-        this.place2.put("latitude", lat2);
-        this.place2.put("longitude", lon2);
-    }
 
     @Override
     public void buildResponse(){
