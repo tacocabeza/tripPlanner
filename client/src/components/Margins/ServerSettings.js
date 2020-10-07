e8171d277aa8d700c0b4bd6ce20aae6bd184dd80
Nicholas Davidson <nicholas.davidson@colostate.edu>
2020-10-07 11:37:29 -0600

    Removing unused lines from file
    Signed-off-by: Nicholas Davidson <nicholas.davidson@colostate.edu>

--- client/src/components/Margins/ServerSettings.js
+++ client/src/components/Margins/ServerSettings.js
@@ -4,7 +4,6 @@ import { Button, Col, Input, Modal, ModalBody, ModalFooter, ModalHeader, Row } f
 import { sendServerRequest, isJsonResponseValid } from "../../utils/restfulAPI";
 
 import * as configSchema from "../../../schemas/ResponseConfig";
-import {forEach} from "react-bootstrap/ElementChildren";
 
 export default class ServerSettings extends Component {
 
