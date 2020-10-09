0ca767662d8845f4432d48553510041e241c92f6
Jesus Garcia <jgarc110@rams.colostate.edu>
2020-10-08 22:30:37 -0600

    Add Options.java and it's respcective tests

---
+++ server/src/test/java/com/tco/functionality/TestOptions.java
@@ -0,0 +1,37 @@
+package com.tco.functionality;
+import com.tco.functionality.Options;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestOptions {
+
+    Options options = new Options("Feather Friends: One Last Flight", "3959");
+
+    @Test
+    public void testGetTripTitle()
+    {
+        assertEquals("Feather Friends: One Last Flight", options.getTripTitle());
+    }
+
+    @Test
+    public void testSetTripTitle()
+    {
+        options.setTripTitle("Feather Friends: Chirp Off The Old Block");
+        assertEquals("Feather Friends: Chirp Off The Old Block", options.getTripTitle());
+    }
+
+    @Test
+    public void testGetEarthRadius()
+    {
+        assertEquals("3959", options.getEarthRadius());
+    }
+
+    @Test
+    public void testSetEarthRadius()
+    {
+        options.setEarthRadius("1");
+        assertEquals("1", options.getEarthRadius());
+    }
+
+
+}
