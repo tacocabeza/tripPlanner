5bfe95aa0f02c0deac5d5c133bbcdc29edd4f1f2
Alex Hooten <alexander.hooten@gmail.com>
2020-10-12 20:35:27 -0600

    finished skeleton of trip UI

--- client/src/components/Atlas/Trip.js
+++ client/src/components/Atlas/Trip.js
@@ -1,5 +1,14 @@
 import React, { Component } from 'react';
-import {Row, Col, Button, Input, ListGroup, ListGroupItem} from "reactstrap";
+import {Row, Col, Button, Input, ListGroup, ListGroupItem, Modal, ModalBody, ModalHeader, ModalFooter, Form} from "reactstrap";
+
+import DeleteIcon from '../../static/images/delete.svg'
+
+const deleteBtn = {
+  background: '#fff',
+  color: '#000',
+  padding: '5px',
+  border: 'none',
+}
 
 export default class Trip extends Component {
   constructor(props) {
@@ -10,14 +19,25 @@ export default class Trip extends Component {
       loadedTrip: {
 
       },
+      destinations: [],
+      destinationModal: false,
+      loadModal: false,
     }
   }
 
   render() {
     return(
-      <div>
-        {this.renderBar()}
-        {this.renderDestinations()}
+      <div className="text-center">
+        <Col xs={12}>
+          <br/>
+          {this.renderBar()}
+          <br/>
+          {this.renderDestinations()}
+          <p className="text-right">Total Distance: 000mi.</p>
+          <Button color="primary" onClick={() => {this.setState({destinationModal: true})}}>Add Stop</Button>
+        </Col>
+        {this.renderDestinationModal()}
+        {this.renderLoadModal()}
       </div>
     );
   }
@@ -25,20 +45,25 @@ export default class Trip extends Component {
   renderBar() {
     return(
       <Row>
-        <Col md={10}>
+        <Col xs={12}>
           <Input
             type="text"
+            style={{width: '60%'}}
+            className="float-left"
             name="tripname"
             placeholder="Trip Name"
             value={this.state.tripName}
             onChange={e => this.setState({tripName: e.target.value})}
           />
-        </Col>
-        <Col md={1}>
-          <Button>Save</Button>
-        </Col>
-        <Col md={1}>
-          <Button>Load</Button>
+          <Row style={{width: '40%'}}>
+            <Button color="primary" style={{width: '49%', marginLeft: '1%'}}>Save</Button>
+            <Button color="primary"
+                    style={{width: '49%', marginLeft: '1%'}}
+                    onClick={() => {this.setState({loadModal: true})}}
+            >
+              Load
+            </Button>
+          </Row>
         </Col>
       </Row>
     );
@@ -46,18 +71,57 @@ export default class Trip extends Component {
 
   renderDestinations() {
     return (
-      <Row>
-        <ListGroup>
-            <ListGroupItem>
-              <Col md={10}>
-                Testing
-              </Col>
-              <Col md={1}>
-                X
-              </Col>
-            </ListGroupItem>
-        </ListGroup>
-      </Row>
+      <ListGroup>
+        <ListGroupItem>
+          <Row>
+            <Col className="text-left">Test Name</Col>
+            <Col>
+              <Button style={deleteBtn} className="float-right">
+                <img style={{height: '25px'}} src={DeleteIcon}/>
+              </Button>
+            </Col>
+          </Row>
+        </ListGroupItem>
+        <p className="text-right">Distance: 000mi.</p>
+        <ListGroupItem>
+          <Row>
+            <Col className="text-left">Test Name</Col>
+            <Col>
+              <Button style={deleteBtn} className="float-right">
+                <img style={{height: '25px'}} src={DeleteIcon}/>
+              </Button>
+            </Col>
+          </Row>
+        </ListGroupItem>
+      </ListGroup>
     );
+  }
+
+  renderDestinationModal() {
+    return (
+      <Modal isOpen={this.state.destinationModal}>
+        <ModalHeader>Add Destination</ModalHeader>
+        <ModalBody>
+        </ModalBody>
+        <ModalFooter>
+          <Button color="primary">Confirm</Button>
+          <Button onClick={() => {this.setState({destinationModal: false})}}>Close</Button>
+        </ModalFooter>
+      </Modal>
+    );
+  }
+
+  renderLoadModal() {
+    return (
+      <Modal isOpen={this.state.loadModal}>
+        <ModalHeader>Load Trip</ModalHeader>
+        <ModalBody>
+        </ModalBody>
+        <ModalFooter>
+          <Button color="primary">Load</Button>
+          <Button onClick={() => {this.setState({loadModal: false})}}>Close</Button>
+        </ModalFooter>
+      </Modal>
+    )
   }
 }
