075f0bf7e399cab927ff5bb1f35e7e554d70a9a4
Jesus Garcia <jgarc110@rams.colostate.edu>
2020-10-13 01:05:31 -0600

    implement RequestTrip.java and it's respectives tests

--- server/src/main/java/com/tco/requests/RequestTrip.java
+++ server/src/main/java/com/tco/requests/RequestTrip.java
@@ -1,18 +1,34 @@
 package com.tco.requests;
 
+import com.tco.functionality.Options;
 import com.tco.misc.BadRequestException;
+import com.tco.misc.DistanceCalculator;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class RequestTrip extends RequestHeader{
 
-    Long [] distances;
+    ArrayList<HashMap<String,String>> places;
+    Options options;
+    private Long [] distances;
+
     public RequestTrip(){
         this.requestType = "trip";
+        this.places = new ArrayList<>();
         this.requestVersion = RequestHeader.CURRENT_SUPPORTED_VERSION;
     }
 
 
     @Override
     public void buildResponse(){
-        distances = new Long[]{1L,2L,3L};
+        DistanceCalculator distanceCalculator = new DistanceCalculator();
+
+        distances = distanceCalculator.calculateDistances(places, options);
+    }
+
+    Long[] getDistances()
+    {
+        return distances;
     }
 }
