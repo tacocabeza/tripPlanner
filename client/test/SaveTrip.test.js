85206a97934dd1d2cec642bb52fbddeb39624de2
Jesus Garcia <jgarc110@rams.colostate.edu>
2020-10-15 20:01:39 -0600

    add some initial tests

---
+++ client/test/SaveTrip.test.js
@@ -0,0 +1,48 @@
+import './jestConfig/enzyme.config.js';
+import React from 'react';
+import {shallow} from 'enzyme';
+
+import SaveTrip from "../src/components/Atlas/SaveTrip.js";
+
+
+function testInitialSaveTripState() {
+
+  const saveTrip = shallow(<SaveTrip/>);
+  let actualIsPopUp = saveTrip.state().isPopUp;
+  let expectedisPopUp = false;
+
+  expect(actualIsPopUp).toEqual(expectedisPopUp);
+
+  let expectedSaveName = "";
+
+  let actualSaveName = saveTrip.state().saveName;
+
+  expect(actualSaveName).toEqual(expectedSaveName);
+}
+test("Initial Save State", testInitialSaveTripState);
+
+function testPopup()
+{
+    const saveTrip = shallow(<SaveTrip/>)
+
+    saveTrip.instance().togglePopUp();
+
+    expect(saveTrip.state().isPopUp).toEqual(true);
+
+}
+
+test("Testing Popup", testPopup);
+
+function testLoadPlaces()
+{
+    const saveTrip = shallow(<SaveTrip places={[]}/>)
+
+    let actualPayLoad = saveTrip.instance().loadPlaces();
+
+    let expectedPayload = {"options":{"title":"","earthRadius":"3959.0"},"places":[],"distances":[],"requestType":"find","requestVersion":{"PROTOCOL_VERSION":3}};
+
+    expect(actualPayLoad).toEqual(expectedPayload);
+
+}
+
+test("Testing LoadPlaces", testLoadPlaces);
