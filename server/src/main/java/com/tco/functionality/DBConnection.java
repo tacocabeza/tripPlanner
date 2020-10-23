0212d4890d93d881f41a748b15af600593e19b36
prestondunton <preston.dunton@gmail.com>
2020-10-23 13:51:06 -0600

    Added try with resource statement

--- server/src/main/java/com/tco/functionality/DBConnection.java
+++ server/src/main/java/com/tco/functionality/DBConnection.java
@@ -30,31 +30,30 @@ public class DBConnection {
         }
     }
 
-    private void initForTravis(){
+    private void initForTravis() {
         log.info("Initializing DBConnection for Travis");
         DB_URL = "jdbc:mysql://127.0.0.1/cs314";
         DB_USER = "root";
         DB_PASSWORD = null;
     }
 
-    private void initForDev(){
+    private void initForDev() {
         log.info("Initializing DBConnection for an off-campus machine");
         DB_URL = "jdbc:mysql://127.0.0.1:1870/cs314";
         DB_USER = "cs314-db";
         DB_PASSWORD = "eiK5liet1uej";
     }
 
-    private void initForProd(){
+    private void initForProd() {
         log.info("Initializing DBConnection for production (black-bottle)");
         DB_URL = "jdbc:mysql://faure.cs.colostate.edu/cs314";
         DB_USER = "cs314-db";
         DB_PASSWORD = "eiK5liet1uej";
     }
 
-    public ResultSet querySQL(String sql){
-        try {
-            Connection conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
-            Statement query = conn.createStatement();
+    public ResultSet querySQL(String sql) {
+        try (Connection conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+             Statement query = conn.createStatement()) {
             return query.executeQuery(sql);
         } catch (Exception e){
             log.error("Exception caught when querying sql: \"" + sql + "\":" + e);
